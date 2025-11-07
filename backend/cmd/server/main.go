575084d912c93da90cf819a947c38e33bda99258
atakhtan <takhtanovalb@gmail.com>
2025-11-07 21:03:51 +0500

    changes

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -13,7 +13,7 @@ import (
 func main() {
 	r := gin.Default()
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000", "http://localhost:5173"},
+		AllowOrigins:     []string{"http://localhost:3000", "http://localhost:5173", "http://localhost:8081"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
